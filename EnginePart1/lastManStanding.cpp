70868841d61c7ba0f7bee334cdd0248a06f07e4d
Acerizm <34855234+Acerizm@users.noreply.github.com>
2019-01-26 21:55:54 +0800

    Testing client and game server

--- EnginePart1/lastManStanding.cpp
+++ EnginePart1/lastManStanding.cpp
@@ -8,6 +8,12 @@
 #include "background.h"
 #include <math.h>
 #include "heart.h"
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+
+// Need to link with Ws2_32.lib
+#pragma comment (lib, "Ws2_32.lib")
 #pragma comment(lib, "Winmm.lib")
 
 
